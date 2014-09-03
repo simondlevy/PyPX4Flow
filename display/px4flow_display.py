76bdbfe578d35dbe33cd37673f5aab1a9ee92582
Simon D. Levy <simon.d.levy@gmail.com>
2014-09-03 00:22:00 -0400

    Update px4flow_display.py

--- display/px4flow_display.py
+++ display/px4flow_display.py
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License 
 along with this code.  If not, see <http://www.gnu.org/licenses/>.
-''''
+'''
 
 # Pick your port
 PORT  = '/dev/ttyACM0'          # Linux
