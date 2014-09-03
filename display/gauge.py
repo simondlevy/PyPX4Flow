4e8a08e658ff08c3c04d1f1ddbf4d7a93ba61c7e
Simon D. Levy <simon.d.levy@gmail.com>
2014-09-03 00:25:08 -0400

    Update gauge.py

--- display/gauge.py
+++ display/gauge.py
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License 
 along with this code.  If not, see <http://www.gnu.org/licenses/>.
-''''
+'''
 
 import Tkinter
 
